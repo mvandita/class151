c3c4423c581d3a1be181164a7b813136e53ef55c
mvandita <86829958+mvandita@users.noreply.github.com>
2023-03-22 19:56:51 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("move-box",{
+    schema:{
+        moveX:{type:"number",default:1},
+    },
+    tick:function(){
+        this.data.moveX=this.data.moveX+5
+        var pos=this.el.getAttribute("position")
+        pos.x=this.data.moveX
+        this.el.setAttribute("position",{x:pos.x,y:pos.y,z:pos.z})
+        var rotate=this.el.getAttribute("rotation")
+        rotate.x=this.data.moveX
+        rotate.y=this.data.moveX
+        this.el.setAttribute("rotation",{x:rotate.x,y:rotate.y,z:rotate.z})
+    }
+})
